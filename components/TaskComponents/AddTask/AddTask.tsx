8df2305f1cbd047b16886e66fc1895cfd9dddabc
Themba Mtshelwane <Thembamm3@gmail.com>
2024-05-09 12:33:57 +0200

    Send task to database

--- components/TaskComponents/AddTask/AddTask.tsx
+++ components/TaskComponents/AddTask/AddTask.tsx
@@ -3,13 +3,25 @@
 import AddButton from '@/components/Buttons/AddButton/AddButton'
 import { useState } from 'react'
 import styles from './addtask.module.css'
+import { addDoc, collection } from 'firebase/firestore'
+import { db } from '@/firebase/firebaseConfig'
 
 type Props = {}
 const AddTask = (props: Props) => {
   const [task, setTask] = useState('')
+  const [loading, isLoading] = useState(false)
+
   const addTask = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+    isLoading(true)
+    try {
+      const docRef = await addDoc(collection(db, 'tasks'), { task })
+      console.log('Tasks uploaded')
+    } catch (e) {
+      console.error('Error adding document: ', e)
+    }
     setTask('')
+    isLoading(false)
   }
 
   return (
